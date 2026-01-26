d393f0cb58613aa8f21c75b92fcc6b502cabbe9b
suhasnu <suhas3908@gmail.com>
2026-01-26 18:30:32 +0100

    updated dream.py

--- dream.py
+++ dream.py
@@ -1,45 +1,31 @@
+import os
+import sys
 import numpy as np
 import tensorflow as tf
+from PIL import Image 
 from keras.applications import MobileNetV2
+from keras.models import Model
 from keras.applications.mobilenet_v2 import preprocess_input
-from keras import Model
-from PIL import Image
 
-#Configurations
-image_path = 'image_1.jpg'
-layer_name = 'block_9_add'
-steps = 1000
-step_size = 0.01
-
-#Loading the Model
-base_model = MobileNetV2(weights = 'imagenet', include_top = False) #Turning off the top classification layer
-
-#selecting the specific layer
-"""
-#Searching for the layer
-for layer in base_model.layers:
-  print(layer.name)
-"""
-target_layer = base_model.get_layer(layer_name)
-
-#Creating a new model that outputs only activations of that layer
-dream_model = Model(inputs = base_model.input, outputs = target_layer.output)
 
 def preprocess_image(image_path):
-  image = Image.open(image_path)
-  #resize to 224x224
-  image = image.resize((224, 224))
-  #Convert to numpy array
-  image_array = np.array(image)
-  #expanding dimension from (224, 224, 3) to (1, 224, 224, 3)
-  image_array = np.expand_dims(image_array, axis = 0)
-  #scale values between -1 and 1
+  
+  #Loadimg image and converting it into RGB image.
+  original_image = Image.open(image_path).convert('RGB')
+
+  #resize image to 224x224x3, PIL library implicitly handles the 3 channels
+  original_image = original_image.resize((224, 224))
+
+  #Converting the image into numpy array and adding the batch dimension.
+  image_array = np.array(original_image)
+  image_array = np.expand_dims(image_array, axis=0)
+
+  #scale the values between -1 to 1
   image_array = preprocess_input(image_array)
   
-  #print(image_array.shape)
-  return image_array
-
-#preprocess_image(image_path)
+  
+  #convert to tensorflow variable to allow geadient updates
+  return tf.Variable(image_array)
 
 def deprocess_image(processed_image):
   x = processed_image.copy()
@@ -54,54 +40,68 @@ def deprocess_image(processed_image):
   x = np.clip(x, 0, 255).astype('uint8')
   return x
 
-#Loss function
-def calculate_loss(layer_activations):
-  #calculating the sum of aquared activation
-  #square the activations to make negative values exciting too
-  losses = tf.square(layer_activations)
-  #sum up all the excitement in the layer
-  total_loss = tf.reduce_sum(losses)
-  return total_loss
+#calculating loss
+def calculate_loss(activations):
+  loss = -tf.reduce_mean(tf.square(activations))
+  return loss
 
-#dreaming loop
-@tf.function #to speed up execution
-def dream_step(image, model, step_size):
-  #calculating Gradient
+#passing arguments
+if len(sys.argv) < 4:
+  print("Enter valid number of arguments")
+  sys.exit(1)
+  
+image_path = sys.argv[1]
+layer = sys.argv[2]
+steps = sys.argv[3]
+N = int(steps)
+
+#Loading the model "turning the off the top classification layer"
+base_model = MobileNetV2(weights = 'imagenet', include_top = False)
+
+#selecting the specific layer
+layer_id = int(layer)
+if layer_id < 0 or layer_id >= len(base_model.layers):
+  print("Layer out of bound")
+  sys.exit(1)
+target_layer = base_model.layers[layer_id]
+#Creating a new model that outputs only activations of that layer
+dream_model = Model(inputs=base_model.input, outputs=target_layer.output)
+
+step_size = 0.01
+
+original_image = preprocess_image(image_path)
+
+@tf.function
+def dream_step(image):
+  #calculating gradients and loss
   with tf.GradientTape() as tape:
     tape.watch(image)
-    activations = model(image)
+    activations = dream_model(image)
     loss = calculate_loss(activations)
-  
+    
   gradients = tape.gradient(loss, image)
   
   #normalizing gardients to avoid exploding/vanishing updates
   gradients /= (tf.math.reduce_std(gradients) + 1e-8)
   
   #update image
-  image = image + (gradients * step_size)
-  
+  image.assign_sub(gradients * step_size)
   #Clip pixel values to stay within the valid range (-1, 1)
-  image = tf.clip_by_value(image, -1, 1)
   
-  return loss, image
+  image.assign(tf.clip_by_value(image, -1, 1))
+  
+  return loss
 
-#exceution part
-image = preprocess_image(image_path)
-image = tf.convert_to_tensor(image) #convert to Tensor for GradientTape
-
-print("Starting to dream")
-
-for step in range(steps):
-  loss, image = dream_step(image, dream_model, step_size)
+for step in range(N):
+  loss_value = dream_step(original_image)
   
   #Displaying progress every 10 steps
   if step % 10 == 0:
-    print(f"Step {step}/{steps}, loss: {loss:.2f}")
-    
+    print(f"Step {step}/{steps}, loss: {loss_value:.2f}")
+
 #save the final result
-dream_image = deprocess_image(image.numpy())
-output_filename = f"dream_{image_path}"
+dream_image = deprocess_image(original_image.numpy())
+output_filename = f"dream_{os.path.basename(image_path)}"
 Image.fromarray(dream_image).save(output_filename)
 
-print(f"Dreaming complete")
-  
+print("Dreaming complete")
